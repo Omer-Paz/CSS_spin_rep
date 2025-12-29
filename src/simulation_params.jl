0a0822f3efbf40c1eab5f73c446ce476f58b8cc7
Omer-Paz <omer.paz2@mail.huji.ac.il>
2025-12-29 14:36:54 +0200

    nm sweep factor fix

--- src/simulation_params.jl
+++ src/simulation_params.jl
@@ -1,9 +1,9 @@
 # Auto-generated parameters file for Z2 Gauge Theory
 geo_name = "psl_2_4"
-betas = [2.0]
-hs = [0.5]
-Jz = [1.0]
-epsilons = [0.0625]
-nm_meas = [1]
+betas = [2.0, 4.0, 8.0, 16.0]
+hs = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
+Jz = [1.0, 1.0]
+epsilons = [0.125, 0.0625, 0.03125, 0.015625]
+nm_meas = [16384]
 nm_sweep_factor = [1]
-nm_therm = [1]
+nm_therm = [1024]
